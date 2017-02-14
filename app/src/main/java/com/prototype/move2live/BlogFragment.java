ea03de0a27e6b9dc5df9d6f7435de04fd41540c9
KirTan <patelkirtann@gmail.com>
2017-02-14 05:11:33 -0800

    onCreate

--- app/src/main/java/com/prototype/move2live/BlogFragment.java
+++ app/src/main/java/com/prototype/move2live/BlogFragment.java
@@ -1,11 +1,11 @@
 package com.prototype.move2live;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 /**
  * Created by kt_ki on 2/7/2017.
